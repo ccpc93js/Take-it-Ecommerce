69c87ab14f3c0f41165f35056ebee463e76e1a5c
cristian palomeque <cristianpalocseres@gmail.com>
2021-01-27 17:20:14 -0500

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import {BrowserRouter, Route} from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
+function App() {
+  return (
+      <BrowserRouter>
+    <div className="grid-container">
+    <header className="row">
+        <div>
+            <a className="brand" href="/">AmazonReact</a>
+        </div>
+        <div>
+            <a href="/cartl">Cart</a>
+            <a href="/signin">Sign in</a>
+        </div>
+    </header>
+
+    <main>
+
+        <Route path="/product/:id" component={ProductScreen}></Route>
+        <Route path="/" component={HomeScreen} exact></Route>
+
+        
+    </main>
+
+    <footer className="row center">
+        All right reserved
+    </footer>
+</div>
+</BrowserRouter>
+  );
+}
+
+export default App;
