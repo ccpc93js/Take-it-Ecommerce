afe6973df06b3ce86539cdc500377ffb3b762bc2
cristian palomeque <cristianpalocseres@gmail.com>
2021-02-02 16:39:48 -0500

    redux productscreen

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,4 +1,4 @@
-const {PRODUCT_LIST_REQUEST,PRODUCT_LIST_SUCCESS,PRODUCT_LIST_FAIL} = require('../constants/productConstants')
+const {PRODUCT_LIST_REQUEST,PRODUCT_LIST_SUCCESS,PRODUCT_LIST_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL} = require('../constants/productConstants')
 
 export const productListReducer = (
   state = { loading: true, products: [] },
@@ -15,6 +15,19 @@ export const productListReducer = (
         page: action.payload.page,
       };
     case PRODUCT_LIST_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const productDetailsReducer = (state = { loading: true }, action) => {
+  switch (action.type) {
+    case PRODUCT_DETAILS_REQUEST:
+      return { loading: true };
+    case PRODUCT_DETAILS_SUCCESS:
+      return { loading: false, product: action.payload };
+    case PRODUCT_DETAILS_FAIL:
       return { loading: false, error: action.payload };
     default:
       return state;
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,37 +1,37 @@
-import React, { useEffect, /*useState*/ } from 'react'
-// import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 // import data from '../data'
 import Product from '../components/Product';
 import MessageBox from '../components/MessageBox';
 import LoadingBox from '../components/LoadingBox';
-import {useDispatch,useSelector } from 'react-redux'
-import {listProducts} from '../actions/productActions'
+// import {useDispatch,useSelector } from 'react-redux'
+// import {listProducts} from '../actions/productActions'
 
 export default function HomeScreen() {
-    // const [products, setProducts]=useState([]);
-    // const [loading, setLoading] = useState(false);
-    // const [error, setError] = useState(false);
-    const dispatch = useDispatch();
-    const productList = useSelector((state)=> state.productList);
-    const {loading, error, products} = productList;
+    const [products, setProducts]=useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+    // const dispatch = useDispatch();
+    // const productList = useSelector((state)=> state.productList);
+    // const {loading, error, products} = productList;
     useEffect(()=>{
-        // const fecthData = async () =>{
-        //     try{
+        const fecthData = async () =>{
+            try{
                 
-        //         setLoading(true);
+                setLoading(true);
         
-        //             const {data} = await axios.get('/api/products');
-        //             setLoading(false);
-        //             setProducts(data);
-        //     }catch(err){
-        //         setError(err.message);
-        //         setLoading(false);
-        //     }
-        // };
-        // fecthData();
+                    const {data} = await axios.get('/api/products');
+                    setLoading(false);
+                    setProducts(data);
+            }catch(err){
+                setError(err.message);
+                setLoading(false);
+            }
+        };
+        fecthData();
 
-        dispatch(listProducts());
-    },[dispatch]);
+        // dispatch(listProducts());
+    },[]/*[dispatch]*/);
     return (
         <div>
             {
